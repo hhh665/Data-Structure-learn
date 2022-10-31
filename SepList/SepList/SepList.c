329c80cf19fe4f201ca8eb34f66678e6919dea60
h665 <594747821@qq.com>
2022-10-31 21:47:23 +0800

    优化代码

--- SepList/SepList/SepList.c
+++ SepList/SepList/SepList.c
@@ -2,10 +2,6 @@
 
 #include "SepList.h"
 
-int BinarySearch(SL* ps, int x)
-{
-
-}
 
 void SLInit(SL* ps)
 {
@@ -36,26 +32,7 @@ void SLPrint(SL* ps)
 	printf("\n");
 }
 
-void SLPushBack(SL* ps, SLDataType x)
-{
-	assert(ps);
-	if (ps->size == ps->capacity)
-	{
-		int Newcapacity = ps->capacity == 0 ? 4 : ps->capacity * 2;
-		SLDataType* tmp = (SLDataType*)realloc(ps->a, Newcapacity*sizeof(SLDataType));
-		if (tmp == NULL)
-		{
-			perror("realloc fail");
-			exit(-1);
-		}
-		ps->a = tmp;
-		ps->capacity = Newcapacity;
-	}
-	ps->a[ps->size] = x;
-	ps->size++;
-}
-
-void SLPushFront(SL* ps, SLDataType x)
+void SLCheckCapacity(SL* ps)
 {
 	assert(ps);
 	if (ps->size == ps->capacity)
@@ -70,31 +47,50 @@ void SLPushFront(SL* ps, SLDataType x)
 		ps->a = tmp;
 		ps->capacity = Newcapacity;
 	}
+}
+
+void SLPushBack(SL* ps, SLDataType x)
+{
+	/*assert(ps);
+	CheckCapacity(ps);
+	ps->a[ps->size] = x;
+	ps->size++;*/
+	SLInsert(ps, ps->size, x);
+}
+
+void SLPushFront(SL* ps, SLDataType x)
+{
+	/*assert(ps);
+	CheckCapacity(ps);
 	for (int i = ps->size; i > 0; i--)
 	{
 		ps->a[i] = ps->a[i-1];
 	}
 	ps->a[0] = x;
-	ps->size++;
+	ps->size++;*/
+	SLInsert(ps, 0, x);
 }
 
 void SLPopFront(SL* ps)
 {
-	assert(ps);
+	/*assert(ps);
 	assert(ps->size > 0);
 	for (int i = 0; i < ps->size; i++)
 	{
 		ps->a[i] = ps->a[i + 1];
 	}
-	ps->size--;
+	ps->size--;*/
+	SLErase(ps, 0);
 }
 
 void SLPopBack(SL* ps)
 {
-	assert(ps);
+	/*assert(ps);
 	assert(ps->size > 0);
 
-	ps->size--;
+	ps->size--;*/
+
+	SLErase(ps, ps->size-1);
 }
 
 int SLFind(SL* ps, SLDataType x)
@@ -102,44 +98,31 @@ int SLFind(SL* ps, SLDataType x)
 	assert(ps);
 	int begin = 0;
 	int end = ps->size - 1;
-	while (begin < end)
+	int i = 0;
+	for (i = 0; i < ps->size; i++)
 	{
-		int mid = begin + ((end - begin) >> 1);
-		if (ps->a[mid] < x)
-		{
-			begin++;
-		}
-		else if (ps->a[mid] > x)
-		{
-			end--;
-		}
-		else
-		{
-			return mid;
-		}
+		if (ps->a[i] == x)
+			return i;
 	}
 	return -1;
 }
 
-void SLInsert(SL* ps, size_t pos, SLDataType x)
+void SLInsert(SL* ps, int pos, SLDataType x)
 {
 	assert(ps);
-	if (ps->size == ps->capacity)
+	assert(pos >= 0);
+	assert(pos <= ps->size);
+	SLCheckCapacity(ps);
+	//int i = 0;
+	/*for (i = ps->size-1; i >= pos; i--)
 	{
-		int Newcapacity = ps->capacity == 0 ? 4 : ps->capacity * 2;
-		SLDataType* tmp = (SLDataType*)realloc(ps->a, Newcapacity * sizeof(SLDataType));
-		if (tmp == NULL)
-		{
-			perror("realloc fail");
-			exit(-1);
-		}
-		ps->a = tmp;
-		ps->capacity = Newcapacity;
-	}
-	int i = 0;
-	for (i = ps->size; i > pos; i--)
+		ps->a[i + 1] = ps->a[i];
+	}*/
+	int end = ps->size - 1;
+	while (end >= pos)
 	{
-		ps->a[i] = ps->a[i - 1];
+		ps->a[end + 1] = ps->a[end];
+		end--;
 	}
 	ps->a[pos] = x;
 	ps->size++;
@@ -148,7 +131,8 @@ void SLInsert(SL* ps, size_t pos, SLDataType x)
 void SLErase(SL* ps, size_t pos)
 {
 	assert(ps);
-	assert(ps->size > 0);
+	assert(pos >= 0);
+	assert(pos < ps->size);
 	for (int i = pos; i < ps->size; i++)
 	{
 		ps->a[i] = ps->a[i + 1];
--- SepList/SepList/test.c
+++ SepList/SepList/test.c
@@ -5,8 +5,6 @@
 enum Option
 {
 	EXIT,
-	INIT,
-	DEST,
 	PRINT,
 	PUSHB,
 	PUSHF,
@@ -20,20 +18,23 @@ enum Option
 void menu()
 {
 	printf("**************************\n");
-	printf("**  1.Init      2.Dest  **\n");
-	printf("**  3.Print     4.PushB **\n");
-	printf("**  5.PushF     6.PopF  **\n");
-	printf("**  7.PopB      8.Find  **\n");
-	printf("**  9.Insert   10.Erase **\n");
+	printf("**  1.Print     2.PushB **\n");
+	printf("**  3.PushF     4.PopF  **\n");
+	printf("**  5.PopB      6.Find  **\n");
+	printf("**  7.Insert    8.Erase **\n");
 	printf("**         0.exit       **\n");
 	printf("**************************\n");
+	printf("请输入序号：\n");
 }
 
 int main()
 {
 	int n = 0;
 	int ret = 0;
+	int e = 0;
+	int x = 0;
 	SL sl;
+	SLInit(&sl);
 	do
 	{
 		menu();
@@ -43,23 +44,19 @@ int main()
 		case EXIT:
 			printf("退出程序\n");
 			break;
-		case INIT:
-			SLInit(&sl);
-			printf("初始化成功\n");
-			break;
-		case DEST:
-			SLDestory(&sl);
-			printf("销毁成功\n");
-			break;
 		case PRINT:
 			SLPrint(&sl);
 			break;
 		case PUSHB:
-			SLPushBack(&sl, 1);
+			printf("请输入要尾插的值：\n");
+			scanf("%d", &e);
+			SLPushBack(&sl, e);
 			printf("尾插成功\n");
 			break;
 		case PUSHF:
-			SLPushFront(&sl, 2);
+			printf("请输入要头插的值：\n");
+			scanf("%d", &e);
+			SLPushFront(&sl, e);
 			printf("头插成功\n");
 			break;
 		case POPF:
@@ -71,8 +68,10 @@ int main()
 			printf("尾删成功\n");
 			break;
 		case FIND:
-			ret = SLFind(&sl, 1);
-			if ( ret == -1)
+			printf("请输入要寻找的值：\n");
+			scanf("%d", &e);
+			ret = SLFind(&sl, e);
+			if (ret == -1)
 			{
 				printf("不存在\n");
 			}
@@ -82,11 +81,17 @@ int main()
 			}
 			break;
 		case INSERT:
-			SLInsert(&sl, 2, 3);
+			printf("请输入要插入值的位置：\n");
+			scanf("%d", &x);
+			printf("请输入要插入的值：\n");
+			scanf("%d", &e);
+			SLInsert(&sl, x, e);
 			printf("插入成功\n");
 			break;
 		case ERASE:
-			SLErase(&sl, 3);
+			printf("请输入要删除的值的位置：\n");
+			scanf("%d", &x);
+			SLErase(&sl, x);
 			printf("删除成功\n");
 			break;
 		default:
@@ -94,5 +99,6 @@ int main()
 			break;
 		}
 	} while (n);
+	SLDestory(&sl);
 	return 0;
 }
