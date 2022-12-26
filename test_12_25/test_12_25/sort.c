746cc6bf60cecc05c5dd5452238d0873d92cb781
h665 <594747821@qq.com>
2022-12-26 16:27:13 +0800

    提交代码

--- test_12_25/test_12_25/sort.c
+++ test_12_25/test_12_25/sort.c
@@ -21,3 +21,104 @@ void InsertSort(int* a, int n)
 		a[end + 1] = tmp;
 	}
 }
+
+void ShellSort(int* a, int n)
+{
+	int gap = n;
+	while (gap > 1)
+	{
+		gap = gap / 3 + 1;
+		for (int i = 0; i < n - gap; i++)
+		{
+			int end = i;
+			int tmp = a[end + gap];
+			while (end >= 0)
+			{
+				if (tmp < a[end])
+				{
+					a[end + gap] = a[end];
+					end -= gap;
+				}
+				else
+				{
+					break;
+				}
+			}
+			a[end + gap] = tmp;
+		}
+	}
+}
+
+void Swap(int* e1, int* e2)
+{
+	int tmp = *e1;
+	*e1 = *e2;
+	*e2 = tmp;
+}
+
+void SelectSort(int* a, int n)
+{
+	int begin = 0;
+	int end = n - 1;
+	while (begin < end)
+	{
+		int maxi = begin;
+		int mini = begin;
+		for (int i = begin + 1; i <= end; i++)
+		{
+			if (a[i] > a[maxi])
+			{
+				maxi = i;
+			}
+			if (a[i] < a[mini])
+			{
+				mini = i;
+			}
+		}
+		Swap(&a[begin], &a[mini]);
+		if (maxi == begin)
+		{
+			maxi = mini;
+		}
+		Swap(&a[end], &a[maxi]);
+		begin++;
+		end--;
+	}
+}
+
+void AdjustDown(int* a, int n, int parent)
+{
+	int child = parent * 2 + 1;
+	while (child < n)
+	{
+		if (child + 1 < n && a[child + 1] > a[child])
+		{
+			child++;
+		}
+		if (a[parent] < a[child])
+		{
+			Swap(&a[parent], &a[child]);
+			parent = child;
+			child = parent * 2 + 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+void HeapSort(int* a, int n)
+{
+	for (int i = (n - 1 - 1) / 2; i >= 0; i--)
+	{
+		AdjustDown(a, n, i);
+	}
+	int end = n - 1;
+	while (end > 0)
+	{
+		Swap(&a[end], &a[0]);
+		AdjustDown(a, end, 0);
+		end--;
+	}
+}
--- test_12_25/test_12_25/sort.h
+++ test_12_25/test_12_25/sort.h
@@ -4,3 +4,7 @@
 
 
 void InsertSort(int* a, int n);
+
+void ShellSort(int* a, int n);
+
+void SelectSort(int* a, int n);
